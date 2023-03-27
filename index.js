6d0f7787c6981200fcdd38a8206d057f09766a26
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-03-27 22:30:26 +0300

    Done Filter

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter((driver)=>{
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+
+}
+function fuzzyMatch(drivers,name){
+  return drivers.filter((driver)=>{
+    return driver.substring(0,name.length) === name
+  })
+}
+function matchName(drivers, name){
+    return drivers.filter((driver)=>{
+        return driver.name === name
+    })
+}
+
+
